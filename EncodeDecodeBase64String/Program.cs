9fdd8c3836c3f8e1a14ed0a13b876cc9a5bb7782
Md. Shakil Reza <rezaspro@gmail.com>
2019-07-24 17:26:24 +0600

    Initial commit

---
+++ EncodeDecodeBase64String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodeDecodeBase64String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encode = Base64Encode("1040100004211208");
+            var decode = Base64Decode(encode);
+
+            Console.WriteLine("{0} {1}", encode, decode);
+            Console.ReadLine();
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
